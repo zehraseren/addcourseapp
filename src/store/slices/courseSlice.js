c969b32ca3ea3c253348971a5be285b940065a3b
Zehra Şeren <fatmazehraseren@gmail.com>
2024-01-28 01:23:39 +0300

    Store ve Slice entegrasyonları yapıldı.

---
+++ src/store/slices/courseSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const courseSlice = createSlice({
+  name: "form",
+  initialState: {
+    searchTerm: "",
+    data: [],
+  },
+  reducers: {},
+});
+
+export const courseReducer = courseSlice.reducer;
---
+++ src/store/slices/formSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const formSlice = createSlice({
+  name: "form",
+  initialState: {
+    name: "",
+    description: "",
+    cost: 0,
+  },
+  reducers: {},
+});
+
+export const formReducer = formSlice.reducer;
